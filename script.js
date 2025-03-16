a5c1a767e5e2819effb9feefcb5b235ffa831cdf
Hatim Mohd Alsharari <hatim.alsharari@tarteebtravel.com>
2025-03-16 14:41:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('demo3').innerHTML='<h6>hello java script</h6>'
+//alert("!")
+let x, y, z;
+x = 10;
+y = 10;
+z = x + y;
+console.log(z);
+//------------------------------------
