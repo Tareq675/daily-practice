c602e9f0ea871b4b752337f4a3e90a270c0a991e
Tareq675 <thtalukder@gmail.com>
2025-03-16 18:35:58 +0300

    array declare

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('demo3').innerHTML='<h6>hello java script</h6>'
+//document.getElementById("demo3").innerHTML = "<h6>hello java script</h6>";
 //alert("!")
 let x, y, z;
 x = 10;
@@ -6,3 +6,12 @@ y = 10;
 z = x + y;
 console.log(z);
 //------------------------------------
+
+//declare array :
+let products = ["Milk Powder", "Orange Tank", "Chocolet"];
+console.log(products);
+products.push("Dates");
+products.push("Olive Oil", "Showder Gel");
+console.log(products);
+products.pop();
+console.log(products);
