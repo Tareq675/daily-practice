4b336e814440155e3e3062b27168ed0c95622643
Tareq675 <thtalukder@gmail.com>
2025-03-18 04:19:45 +0300

    array push, pop, unshif, shift,for of & for loop

--- script.js
+++ script.js
@@ -4,14 +4,36 @@ let x, y, z;
 x = 10;
 y = 10;
 z = x + y;
-console.log(z);
+// console.log(z);
 //------------------------------------
 
 //declare array :
 let products = ["Milk Powder", "Orange Tank", "Chocolet"];
-console.log(products);
-products.push("Dates");
-products.push("Olive Oil", "Showder Gel");
-console.log(products);
-products.pop();
-console.log(products);
+let number = [10, 15, 20, 30, 50];
+// console.log(number);
+
+// console.log(products);
+// products.push("Dates");
+// products.push("Olive Oil", "Showder Gel");
+// console.log(products);
+// products.pop();
+// console.log(products);
+// products.unshift("pen", 15);
+// console.log(products);
+// products.shift();
+// console.log(products);
+// products[0] = 15;
+// console.log(products);
+
+let arrayConcat = products.concat(number);
+console.log(arrayConcat);
+
+// for of loop
+
+// for (let product of products) {
+//   console.log(product);
+// }
+
+// for (let i = 0; i < products.length; i++) {
+//   console.log(i);
+// }
